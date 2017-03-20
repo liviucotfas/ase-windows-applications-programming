e81a31ac26dd614f4a7b5900e4540cad8cc64013
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2017-03-20 17:37:01 +0200

    Removed unnecessary namespace from the usings list

--- code samples/ValidationExceptionHandling/Program.cs
+++ code samples/ValidationExceptionHandling/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Validation;
 using ValidationCustomExceptions;
 
 namespace ValidationExceptionHandeling
