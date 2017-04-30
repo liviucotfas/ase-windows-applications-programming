bb6de20b24e983e761461c519467d8d524a0f97b
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2017-04-30 19:06:23 +0300

    Added BarChart Sample

---
+++ code samples/BarChartGraphicsSample/BarChartControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using BarChartGraphicsSample.Entities;
+
+namespace BarChartGraphicsSample
+{
+	public partial class BarChartControl : UserControl
+	{
+		#region Properties
+		private BarChartValue[] _data;
+		public BarChartValue[] Data
+		{
+			get { return _data; }
+			set
+			{
+				if (_data == value)
+					return;
+
+				_data = value;
+
+				//trigger the Paint event
+				Invalidate();
+			}
+		}
+		#endregion
+
+		public BarChartControl()
+		{
+			InitializeComponent();
+
+			//redraws the chart if the control is resized
+			ResizeRedraw = true;
+
+			//default data
+			Data = new[]
+			{
+				new BarChartValue("2015", 30), 
+				new BarChartValue("2016", 80),
+				new BarChartValue("2017", 40)
+			};
+		}
+
+		private void BarChartControl_Paint(object sender, PaintEventArgs e)
+		{
+			//get the drawing context
+			Graphics graphics = e.Graphics;
+			//get the drawing area
+			Rectangle clipRectangle = e.ClipRectangle;
+
+			//determine the width and the height
+			float width = clipRectangle.Width;
+			float height = clipRectangle.Height;
+
+			var barWidth = width / Data.Length;
+			var maxBarHeight = height * 0.9;
+			var scalingFactor = maxBarHeight / Data.Max(x=>x.Y);
+			
+			for (int i = 0; i < Data.Length; i++)
+			{
+				Brush b = new SolidBrush(Color.Red);
+
+				var barHeight = (float) (Data[i].Y * scalingFactor);
+
+				graphics.FillRectangle(b, i * barWidth, height - barHeight, 0.8f * barWidth, barHeight);
+			}
+		}	
+	}
+}
---
+++ code samples/BarChartGraphicsSample/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BarChartGraphicsSample
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.barChartControl = new BarChartGraphicsSample.BarChartControl();
+			this.SuspendLayout();
+			// 
+			// barChartControl
+			// 
+			this.barChartControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.barChartControl.Location = new System.Drawing.Point(0, 0);
+			this.barChartControl.Name = "barChartControl";
+			this.barChartControl.Size = new System.Drawing.Size(739, 365);
+			this.barChartControl.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(739, 365);
+			this.Controls.Add(this.barChartControl);
+			this.Name = "MainForm";
+			this.Text = "BarChart Sample";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private BarChartControl barChartControl;
+	}
+}
+
