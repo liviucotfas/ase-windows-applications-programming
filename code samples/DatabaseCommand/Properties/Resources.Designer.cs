b8a7c2a6fe7a6cc9f13469a00be3e2d0f460dc53
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2017-04-30 14:43:43 +0300

    Renamed projects

--- code samples/DatabaseCommand/Properties/Resources.Designer.cs
+++ code samples/DatabaseCommand/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DataBaseCommand.Properties {
+namespace DatabaseCommand.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DataBaseCommand.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataBaseCommand.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DatabaseCommand.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- code samples/DatabaseDataAdapter/Properties/Resources.Designer.cs
+++ code samples/DatabaseDataAdapter/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DataBaseDataAdapter.Properties {
+namespace DatabaseDataAdapter.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DataBaseDataAdapter.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataBaseDataAdapter.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DatabaseDataAdapter.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
