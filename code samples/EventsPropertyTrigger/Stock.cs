e97c69186da428301df12ed5237e7f20889a47ac
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2018-03-19 11:21:03 +0200

    Updated event samples

---
+++ code samples/EventsPropertyTrigger/Stock.cs
@@ -0,0 +1,30 @@
+﻿namespace EventsPropertyTrigger
+{
+	public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+
+	internal class Stock
+	{
+		private string _symbol;
+		private decimal _price;
+
+		public Stock(string symbol)
+		{
+			_symbol = symbol;
+		}
+
+		public event PriceChangedHandler PriceChanged;
+
+		public decimal Price
+		{
+			get { return _price; }
+			set
+			{
+				if (_price == value) return; // Exit if nothing has changed
+				decimal oldPrice = _price;
+				_price = value;
+				if (PriceChanged != null) // If invocation list not
+					PriceChanged(oldPrice, _price); // empty, fire event.
+			}
+		}
+	}
+}
--- code samples/EventsPropertyTriggerEventArgs/Program.cs
+++ code samples/EventsPropertyTriggerEventArgs/Program.cs
@@ -2,46 +2,6 @@
 
 namespace EventsPropertyTriggerEventArgs
 {
-	public class PriceChangedEventArgs : EventArgs
-	{
-		public readonly decimal LastPrice;
-		public readonly decimal NewPrice;
-		public PriceChangedEventArgs(decimal lastPrice, decimal newPrice)
-		{
-			LastPrice = lastPrice;
-			NewPrice = newPrice;
-		}
-	}
-
-	public class Stock
-	{
-		private string _symbol;
-		private decimal _price;
-
-		public Stock(string symbol)
-		{
-			_symbol = symbol;
-		}
-
-		public event EventHandler<PriceChangedEventArgs> PriceChanged;
-		protected virtual void OnPriceChanged(PriceChangedEventArgs e)
-		{
-			if (PriceChanged != null) PriceChanged(this, e);
-		}
-		public decimal Price
-		{
-			get { return _price; }
-			set
-			{
-				if (_price == value) return;
-				decimal oldPrice = _price;
-				_price = value;
-
-				OnPriceChanged(new PriceChangedEventArgs(oldPrice, _price));
-			}
-		}
-	}
-
 	internal class Program
 	{
 		private static void Main()
