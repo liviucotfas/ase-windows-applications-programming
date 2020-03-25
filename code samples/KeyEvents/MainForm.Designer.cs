d8abab2e876a74be5a7a7de708253493afc61743
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2020-03-25 18:47:31 +0200

    Update MainForm.Designer.cs

--- code samples/KeyEvents/MainForm.Designer.cs
+++ code samples/KeyEvents/MainForm.Designer.cs
@@ -28,95 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.lblLower = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.lblUpper = new System.Windows.Forms.Label();
-			this.btnReset = new System.Windows.Forms.Button();
-			this.txtMsg = new System.Windows.Forms.TextBox();
-			this.txtInput = new System.Windows.Forms.TextBox();
-			this.SuspendLayout();
-			// 
-			// lblLower
-			// 
-			this.lblLower.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.lblLower.Location = new System.Drawing.Point(730, 111);
-			this.lblLower.Name = "lblLower";
-			this.lblLower.Size = new System.Drawing.Size(32, 23);
-			this.lblLower.TabIndex = 13;
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(682, 111);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(40, 16);
-			this.label1.TabIndex = 12;
-			this.label1.Text = "Lower:";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(682, 63);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(40, 16);
-			this.label2.TabIndex = 11;
-			this.label2.Text = "Upper:";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// lblUpper
-			// 
-			this.lblUpper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.lblUpper.Location = new System.Drawing.Point(730, 63);
-			this.lblUpper.Name = "lblUpper";
-			this.lblUpper.Size = new System.Drawing.Size(32, 23);
-			this.lblUpper.TabIndex = 10;
-			// 
-			// btnReset
-			// 
-			this.btnReset.Location = new System.Drawing.Point(690, 15);
-			this.btnReset.Name = "btnReset";
-			this.btnReset.Size = new System.Drawing.Size(75, 23);
-			this.btnReset.TabIndex = 9;
-			this.btnReset.Text = "Reset";
-			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
-			// 
-			// txtMsg
-			// 
-			this.txtMsg.Location = new System.Drawing.Point(12, 47);
-			this.txtMsg.Multiline = true;
-			this.txtMsg.Name = "txtMsg";
-			this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.txtMsg.Size = new System.Drawing.Size(652, 232);
-			this.txtMsg.TabIndex = 8;
-			this.txtMsg.TabStop = false;
-			// 
-			// txtInput
-			// 
-			this.txtInput.Location = new System.Drawing.Point(12, 15);
-			this.txtInput.Name = "txtInput";
-			this.txtInput.Size = new System.Drawing.Size(652, 20);
-			this.txtInput.TabIndex = 7;
-			this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
-			this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
-			this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
-			this.txtInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyUp);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(792, 291);
-			this.Controls.Add(this.lblLower);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.lblUpper);
-			this.Controls.Add(this.btnReset);
-			this.Controls.Add(this.txtMsg);
-			this.Controls.Add(this.txtInput);
-			this.Name = "MainForm";
-			this.Text = "Key Event Demonstrator";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.lblLower = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUpper = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblLower
+            // 
+            this.lblLower.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLower.Location = new System.Drawing.Point(730, 111);
+            this.lblLower.Name = "lblLower";
+            this.lblLower.Size = new System.Drawing.Size(32, 23);
+            this.lblLower.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(682, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Lower:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(682, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Upper:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblUpper
+            // 
+            this.lblUpper.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUpper.Location = new System.Drawing.Point(730, 63);
+            this.lblUpper.Name = "lblUpper";
+            this.lblUpper.Size = new System.Drawing.Size(32, 23);
+            this.lblUpper.TabIndex = 10;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(690, 15);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtMsg.Location = new System.Drawing.Point(12, 47);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(652, 232);
+            this.txtMsg.TabIndex = 8;
+            this.txtMsg.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 15);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(652, 20);
+            this.txtInput.TabIndex = 7;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            this.txtInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 291);
+            this.Controls.Add(this.lblLower);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUpper);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtInput);
+            this.Name = "MainForm";
+            this.Text = "Key Event Demonstrator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
