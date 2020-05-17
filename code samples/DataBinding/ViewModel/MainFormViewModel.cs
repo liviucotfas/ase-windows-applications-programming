ee30fae3906ce9c892345d0f6a84070ba1e337b2
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2020-05-17 21:53:17 +0300

    Removed ReSharper specific annotations

--- code samples/DataBinding/ViewModel/MainFormViewModel.cs
+++ code samples/DataBinding/ViewModel/MainFormViewModel.cs
@@ -88,7 +88,6 @@ namespace DataBindingSample.ViewModel
 		#region INotifyPropertyChanged
 		public event PropertyChangedEventHandler PropertyChanged;
 
-		[NotifyPropertyChangedInvocator]
 		// [CallerMemberName] - Allows you to obtain the method or property name of the caller to the method. https://msdn.microsoft.com/en-us/library/system.runtime.compilerservices.callermembernameattribute%28v=vs.110%29.aspx
 		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
