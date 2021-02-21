53fb37622bd596eab23506829356cc8f4534e719
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2021-02-21 14:57:14 +0200

    Removed ReSharper specific code

--- code samples/DataBinding/ViewModel/MainFormViewModel.cs
+++ code samples/DataBinding/ViewModel/MainFormViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using DataBindingDialogs.Annotations;
 using DataBindingSample.Entities;
 
 namespace DataBindingSample.ViewModel
